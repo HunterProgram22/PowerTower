e85c8407f2517bc7436768c957f674a6b4d9ee7c
Justin Kudela <justinkudela@gmail.com>
2022-12-21 15:34:25 -0500

    Updated WaveData.

--- Scenes/Enemies/WaveData.gd
+++ Scenes/Enemies/WaveData.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 var WAVE_DATA = [
-	[["BlueTank", 1.0], ["RedTank", 2.0]],
 	[["BlueTank", 1.0], ["BlueTank", 2.0]],
+	[["BlueTank", 1.0], ["RedTank", 2.0]],
 	[["MasterTank", 1.0], ["RedTank", 1.0]],
 	]
