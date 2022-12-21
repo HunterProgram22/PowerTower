e50de3c4c6f24c134a9ad3c135947569d916ffed
Justin Kudela <justinkudela@gmail.com>
2022-12-20 20:45:25 -0500

    Fixed second turret create bug.

--- Scenes/Turrets/Turrets.gd
+++ Scenes/Turrets/Turrets.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
+var type
 var enemy_array = []
 var built = false
 var enemy
 
 func _ready():
 	if built:
-		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[self.get_name()]["range"]
+		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[type]["range"]
 
 func _physics_process(delta):
 	if enemy_array.size() != 0 and built:
