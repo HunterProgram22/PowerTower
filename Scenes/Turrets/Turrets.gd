1b2fa13d79f59c2795c54257b0123ec87dde7d80
Justin Kudela <justinkudela@gmail.com>
2022-12-20 20:32:16 -0500

    Bug fixed for turrets.

--- Scenes/Turrets/Turrets.gd
+++ Scenes/Turrets/Turrets.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+var enemy_array = []
+var built = false
 
+func _ready():
+	if built:
+		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[self.get_name()]["range"]
 
 func _physics_process(delta):
 	turn()
@@ -9,3 +14,12 @@ func _physics_process(delta):
 func turn():
 	var enemy_position = get_global_mouse_position()
 	get_node("Turret").look_at(enemy_position)
+
+
+func _on_Range_body_entered(body):
+	enemy_array.append(body.get_parent())
+	print(enemy_array)
+
+
+func _on_Range_body_exited(body):
+	enemy_array.erase(body.get_parent())
