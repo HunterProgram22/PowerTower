447537b4783075c250974366e5baba9f4256834b
Justin Kudela <justinkudela@gmail.com>
2022-12-20 20:50:49 -0500

    Added damage and firing.

--- Scenes/Enemies/BlueTank.gd
+++ Scenes/Enemies/BlueTank.gd
@@ -2,6 +2,7 @@ extends PathFollow2D
 
 
 var speed = 150
+var hp = 50
 
 
 func _physics_process(delta):
@@ -11,3 +12,12 @@ func _physics_process(delta):
 func move(delta):
 	set_offset(get_offset() + speed * delta)
 
+
+func on_hit(damage):
+	hp -= damage
+	if hp <= 0:
+		on_destroy()
+
+
+func on_destroy():
+	self.queue_free()
--- Scenes/Turrets/Turrets.gd
+++ Scenes/Turrets/Turrets.gd
@@ -4,6 +4,7 @@ var type
 var enemy_array = []
 var built = false
 var enemy
+var ready = true
 
 func _ready():
 	if built:
@@ -13,6 +14,8 @@ func _physics_process(delta):
 	if enemy_array.size() != 0 and built:
 		select_enemy()
 		turn()
+		if ready:
+			fire()
 	else:
 		enemy = null
 
@@ -28,6 +31,13 @@ func select_enemy():
 	var max_offset = enemy_progress_array.max()
 	var enemy_index = enemy_progress_array.find(max_offset)
 	enemy = enemy_array[enemy_index]
+
+
+func fire():
+	ready = false
+	enemy.on_hit(GameData.tower_data[type]["damage"])
+	yield(get_tree().create_timer(GameData.tower_data[type]["rof"]), "timeout")
+	ready = true
 
 
 func _on_Range_body_entered(body):
