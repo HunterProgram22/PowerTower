21ede9b162121aae841e49ccfd6894c21e557f21
Justin Kudela <justinkudela@gmail.com>
2022-12-20 21:05:22 -0500

    Tutorial Part 9 complete.

--- Scenes/Enemies/BlueTank.gd
+++ Scenes/Enemies/BlueTank.gd
@@ -4,6 +4,13 @@ extends PathFollow2D
 var speed = 150
 var hp = 50
 
+onready var health_bar = get_node("HealthBar")
+
+func _ready():
+	health_bar.max_value = hp
+	health_bar.value = hp
+	health_bar.set_as_toplevel(true)
+
 
 func _physics_process(delta):
 	move(delta)
@@ -11,10 +18,12 @@ func _physics_process(delta):
 
 func move(delta):
 	set_offset(get_offset() + speed * delta)
+	health_bar.set_position(position - Vector2(30, 30))
 
 
 func on_hit(damage):
 	hp -= damage
+	health_bar.value = hp
 	if hp <= 0:
 		on_destroy()
 
