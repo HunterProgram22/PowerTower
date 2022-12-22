990e7ad963ce2fd964ee09dc99ead55a81c7fb25
Justin Kudela <justinkudela@gmail.com>
2022-12-21 19:18:07 -0500

    Starting to add second path.

--- Scenes/Enemies/WaveData.gd
+++ Scenes/Enemies/WaveData.gd
@@ -2,7 +2,13 @@ extends Node
 
 
 var WAVE_DATA = [
-	[["BlueTank", 1.0], ["BlueTank", 2.0]],
-	[["BlueTank", 1.0], ["RedTank", 2.0]],
-	[["MasterTank", 1.0], ["RedTank", 1.0]],
+	[
+		["BlueTank", 1.0], ["BlueTank", 2.0], ["BlueTank", 1.0], ["BlueTank", 0.5]
+	],
+	[
+		["BlueTank", 1.0], ["RedTank", 2.0]
+	],
+	[
+		["MasterTank", 1.0], ["RedTank", 1.0]
+	],
 	]
