77a9b8d7fa63b88e0e2fcc93502b8a322c1e19cb
Justin Kudela <justinkudela@gmail.com>
2022-12-23 21:29:06 -0500

    Set Main menu to go to map menu before game start.

--- SceneHandler.gd
+++ SceneHandler.gd
@@ -13,18 +13,22 @@ func load_main_menu():
 
 func on_new_game_pressed():
 	get_node("MainMenu").queue_free()
-	var game_scene = load("res://Scenes/MainScenes/GameScene.tscn").instance()
-	game_scene.connect("game_finished", self, "unload_game")
-	game_scene.connect("level_completed", self, "load_map_menu")
-	add_child(game_scene)
+	load_map_menu()
 
 
 func on_quit_pressed():
 	get_tree().quit()
 
 
+func start_game():
+	var game_scene = load("res://Scenes/MainScenes/GameScene.tscn").instance()
+	game_scene.connect("game_finished", self, "unload_game")
+	game_scene.connect("level_completed", self, "load_map_menu")
+	add_child(game_scene)
+
+
 func load_map_menu():
-	get_node("GameScene").queue_free()
+#	get_node("GameScene").queue_free()
 	var map_menu = load("res://Scenes/UIScenes/MapMenu.tscn").instance()
 	add_child(map_menu)
 
