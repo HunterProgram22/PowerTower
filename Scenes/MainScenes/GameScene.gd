673694b9fbafd464cbbee94452acb911f426d46c
Justin Kudela <justinkudela@gmail.com>
2022-12-24 18:44:33 -0500

    Refactored _ready function in GameScene to make adding maps easier.

--- Scenes/MainScenes/GameScene.gd
+++ Scenes/MainScenes/GameScene.gd
@@ -20,18 +20,9 @@ var wave_data = []
 
 
 func _ready():
-	if map_node == 'Map1':
-		map_node = load('res://Scenes/Maps/Map1.tscn').instance()
-		add_child(map_node)
-		wave_data = WaveData.wave_data['Map1']
-	elif map_node == 'Map2':
-		map_node = load('res://Scenes/Maps/Map2.tscn').instance()
-		add_child(map_node)
-		wave_data = WaveData.wave_data['Map2']
-	elif map_node == 'Map3':
-		map_node = load('res://Scenes/Maps/Map3.tscn').instance()
-		add_child(map_node)
-		wave_data = WaveData.wave_data['Map3']
+	wave_data = WaveData.wave_data[map_node]
+	map_node = load('res://Scenes/Maps/' + map_node + '.tscn').instance()
+	add_child(map_node)
 	enemies_in_stage = get_total_enemies()
 	for i in get_tree().get_nodes_in_group('build_buttons'):
 		i.connect('pressed', self, 'initiate_build_mode', [i.get_name()])
