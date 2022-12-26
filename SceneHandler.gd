6a68e133ce7a345fe0b96f9954cf62be87f26ba4
Justin Kudela <justinkudela@gmail.com>
2022-12-26 06:18:25 -0500

    Continuing SceneHandler cleanup.

--- SceneHandler.gd
+++ SceneHandler.gd
@@ -1,6 +1,10 @@
 extends Node
 
 
+const GAME_SCENE : String = 'res://Scenes/MainScenes/GameScene.tscn'
+const MAIN_MENU : String = 'res://Scenes/UIScenes/MainMenu.tscn'
+const MAP_MENU : String = 'res://Scenes/UIScenes/MapMenu.tscn'
+
 
 func _ready() -> void:
 	connect_signals()
@@ -14,19 +18,19 @@ func connect_signals() -> void:
 
 
 func load_main_menu() -> void:
-	var main_menu = load('res://Scenes/UIScenes/MainMenu.tscn').instance()
+	var main_menu = load(MAIN_MENU).instance()
 	add_child(main_menu)
 
 
 func load_map_menu() -> void:
 	$MainMenu.queue_free()
-	var map_menu = load('res://Scenes/UIScenes/MapMenu.tscn').instance()
+	var map_menu = load(MAP_MENU).instance()
 	add_child(map_menu)
 
 
-func return_to_map_menu(map: str) -> void:
+func return_to_map_menu(map: String) -> void:
 	$GameScene.queue_free()
-	var map_menu = load('res://Scenes/UIScenes/MapMenu.tscn').instance()
+	var map_menu = load(MAP_MENU).instance()
 	map_menu.map_completed = map
 	add_child(map_menu)
 
@@ -35,9 +39,9 @@ func quit_game() -> void:
 	get_tree().quit()
 
 
-func start_game(map: str) -> void:
+func start_game(map: String) -> void:
 	$MapMenu.queue_free()
-	var game_scene = load('res://Scenes/MainScenes/GameScene.tscn').instance()
+	var game_scene = load(GAME_SCENE).instance()
 	game_scene.map_node = map
 	game_scene.connect('game_finished', self, 'unload_game')
 	game_scene.connect('level_completed', self, 'return_to_map_menu', [map])
@@ -46,5 +50,5 @@ func start_game(map: str) -> void:
 
 func unload_game(result) -> void:
 	$GameScene.queue_free()
-	var main_menu = load('res://Scenes/UIScenes/MainMenu.tscn').instance()
+	var main_menu = load(MAIN_MENU).instance()
 	add_child(main_menu)
