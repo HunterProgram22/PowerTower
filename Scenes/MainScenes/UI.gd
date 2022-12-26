627095fb5856a6715380f050f891ed6aaa5371e3
Justin Kudela <justinkudela@gmail.com>
2022-12-26 16:01:21 -0500

    Cleaning up UI script.

--- Scenes/MainScenes/UI.gd
+++ Scenes/MainScenes/UI.gd
@@ -1,24 +1,29 @@
+# warning-ignore-all:return_value_discarded
 extends CanvasLayer
 
 
-var current_wave_value = 1
-onready var hp_bar = get_node('HUD/InfoBar/H/HP')
-onready var hp_bar_tween = get_node('HUD/InfoBar/H/HP/Tween')
-onready var wave_value = get_node('HUD/InfoBar/H/Wave')
-onready var gun_button = get_node('HUD/BuildBar/Gun')
-onready var missile_button = get_node('HUD/BuildBar/Missile')
+var current_wave_value : int = 1
+var money : int = 150
 
-var money = 150
-onready var current_money = get_node('HUD/InfoBar/H/Money')
+onready var current_money : Node = get_node('HUD/InfoBar/H/Money')
+onready var hp_bar : Node = get_node('HUD/InfoBar/H/HP')
+onready var hp_bar_tween : Node = get_node('HUD/InfoBar/H/HP/Tween')
+onready var wave_value : Node = get_node('HUD/InfoBar/H/Wave')
+onready var gun_button : Node = get_node('HUD/BuildBar/Gun')
+onready var missile_button : Node = get_node('HUD/BuildBar/Missile')
 
 
-func _ready():
+func _ready() -> void:
 	set_money()
 	set_buttons()
+	connect_signals()
+
+
+func connect_signals() -> void:
 	Events.connect('cash_changed', self, 'set_buttons')
 
 
-func set_buttons():
+func set_buttons() -> void:
 	if int(current_money.text) >= 50:
 		gun_button.set_disabled(false)
 	else:
@@ -29,37 +34,47 @@ func set_buttons():
 		missile_button.set_disabled(true)
 
 
-func set_money():
+func set_money() -> void:
 	current_money.text = str(money)
 
 
-func set_tower_preview(tower_type, mouse_position):
-	var drag_tower = load('res://Scenes/Turrets/' + tower_type + '.tscn').instance()
-	drag_tower.set_name('DragTower')
-	drag_tower.modulate = Color('ad54ff3c')
-	
-	var range_texture = Sprite.new()
-	range_texture.position = Vector2(32, 32)
-	var scaling = GameData.tower_data[tower_type]['range'] / 600.0
-	range_texture.scale = Vector2(scaling, scaling)
-	var texture = load('res://Assets/UI/range_overlay.png')
-	range_texture.texture = texture
-	range_texture.modulate = Color('ad54ff3c')
+func set_tower_preview(tower_type: String, mouse_position: Vector2) -> void:
+	var drag_tower = create_drag_tower(tower_type)
+	var tower_range = create_tower_range(tower_type)
 
 	var control = Control.new()
 	control.add_child(drag_tower, true)
-	control.add_child(range_texture, true)
+	control.add_child(tower_range, true)
 	control.rect_position = mouse_position
 	control.set_name('TowerPreview')
 	add_child(control, true)
 	move_child(get_node('TowerPreview'), 0)
 
 
-func update_tower_preview(new_position, color):
+func update_tower_preview(new_position: Vector2, color: String) -> void:
 	get_node('TowerPreview').rect_position = new_position
 	if get_node('TowerPreview/DragTower').modulate != Color(color):
 		get_node('TowerPreview/DragTower').modulate = Color(color)
 		get_node('TowerPreview/Sprite').modulate = Color(color)
+
+
+func create_drag_tower(tower_type: String) -> Node2D:
+	"""Creates image of tower to drag on map for building."""
+	var tower = load('res://Scenes/Turrets/' + tower_type + '.tscn').instance()
+	tower.set_name('DragTower')
+	tower.modulate = Color('ad54ff3c')
+	return tower
+
+
+func create_tower_range(tower_type: String) -> Sprite:
+	"""Creates an image that shows the towers range."""
+	var range_texture = Sprite.new()
+	range_texture.position = Vector2(32, 32)
+	var scaling = GameData.tower_data[tower_type]['range'] / 600.0
+	range_texture.scale = Vector2(scaling, scaling)
+	range_texture.texture = load('res://Assets/UI/range_overlay.png')
+	range_texture.modulate = Color('ad54ff3c')
+	return range_texture
 
 
 ##
