d9ef987878ab873402a448bc2149b0ec139bd68f
Justin Kudela <justinkudela@gmail.com>
2022-12-26 21:19:22 -0500

    Bug is with inheritance from BlueTank Scene not going to Master - Base
    Needed.

--- Scenes/Enemies/WaveData.gd
+++ Scenes/Enemies/WaveData.gd
@@ -6,7 +6,7 @@ var wave_data = {
 	[
 		[['BlueTank', 1.0], ['BlueTank', 2.0], ['BlueTank', 1.0], ['BlueTank', 0.5],],
 		[['BlueTank', 1.0], ['RedTank', 2.0],],
-		[['BlueTank', 1.0], ['RedTank', 1.0], ['MasterTank', 1.0], ['RedTank', 1.0],],
+		[['BlueTank', 1.0], ['RedTank', 1.0], ['MasterTank', 1.0], ['MasterTank', 1.0],],
 	],
 	'Map2':
 	[
