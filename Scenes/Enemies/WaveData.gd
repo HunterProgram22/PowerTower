b60a5f2ff10fa31f03ef11776df5656dd8532e11
Justin Kudela <justinkudela@gmail.com>
2023-01-01 19:10:58 -0500

    Changed WaveData.

--- Scenes/Enemies/WaveData.gd
+++ Scenes/Enemies/WaveData.gd
@@ -1,23 +1,37 @@
 extends Node
 
+const BT: String = 'BlueTank'
+const RT: String = 'RedTank'
+const MT: String = 'MasterTank'
+
+const F: float = 0.5
+const M: float = 1.0
+const S: float = 2.0
+
+const FIVE_BLUE_M: Array = [[BT, M], [BT, M], [BT, M], [BT, M], [BT, M]]
+const THREE_RED_F: Array = [[RT, F], [RT, F], [RT, F]]
+const TWO_MASTER_S: Array = [[MT, S], [MT, S]]
 
 var wave_data = {
 	'Map1':
 	[
-		[['BlueTank', 1.0], ['BlueTank', 2.0], ['BlueTank', 1.0], ['BlueTank', 0.5],],
-		[['BlueTank', 1.0], ['RedTank', 2.0],],
-		[['BlueTank', 1.0], ['RedTank', 1.0], ['MasterTank', 1.0], ['MasterTank', 1.0],],
+		TWO_MASTER_S,
+		THREE_RED_F,
+		[[BT, M], [BT, S], [BT, M], [BT, M],],
+		FIVE_BLUE_M,
+		[[BT, M], [RT, S], [RT, M]],
+		[[BT, M], [RT, M], [RT, M], [MT, M],],
 	],
 	'Map2':
 	[
-		[['MasterTank', 1.0], ['BlueTank', 2.0], ['BlueTank', 1.0], ['BlueTank', 0.5],],
-		[['RedTank', 1.0], ['RedTank', 2.0], ['RedTank', 1.0],],
-		[['MasterTank', 1.0], ['RedTank', 1.0], ['MasterTank', 1.0], ['MasterTank', 1.0],],
+		[[MT, M], [BT, S], [BT, M], [BT, M],],
+		[[RT, M], [RT, S], [RT, M],],
+		[[MT, M], [RT, M], [MT, M], [MT, M],],
 	],
 	'Map3':
 	[
-		[['MasterTank', 1.0], ['BlueTank', 2.0], ['BlueTank', 1.0], ['BlueTank', 0.5],],
-		[['MasterTank', 1.0], ['BlueTank', 2.0], ['RedTank', 1.0],],
-		[['MasterTank', 1.0], ['RedTank', 1.0], ['MasterTank', 1.0], ['MasterTank', 1.0],],
+		[[MT, M], [BT, S], [BT, M], [BT, M],],
+		[[MT, M], [BT, S], [RT, M],],
+		[[MT, M], [RT, M], [MT, M], [MT, M],],
 	],
 }
